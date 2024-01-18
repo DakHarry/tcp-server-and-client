090d2e4d287be51ac75f47ff2cf43aeb44e488a9
Harry Jhan <monkey030210@gmail.com>
2024-01-17 22:56:25 -0500

    feat: if received three times execute context cancel

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tcp-server-and-client
+
+
+## TCP Server
+
+- Server can receive the message and response the message to the client.
+- Server can broadcast the message to the clients.
+- Server can graceful shutdown the service.
+- 
--- tcp-server/main.go
+++ tcp-server/main.go
@@ -1,37 +1,74 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"net"
+	"sync"
 	"time"
 )
 
+type TaskMgr struct {
+	proccessedTaskCount int
+	maxConcurrency      int
+	mu                  sync.Mutex
+}
+
+func (mgr *TaskMgr) Count() {
+	mgr.mu.Lock()
+	mgr.proccessedTaskCount += 1
+	mgr.mu.Unlock()
+}
+
 func main() {
 	fmt.Println("Start TCP Server...")
+	ctx, cancel := context.WithCancel(context.Background())
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
+	defer ln.Close()
+
+	mgr := &TaskMgr{
+		proccessedTaskCount: 0,
+		maxConcurrency:      100,
+	}
+
+	wg := new(sync.WaitGroup)
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println("Accept error:", err)
-			continue
+			break
 		}
-
-		go handleMessage(conn)
+		if mgr.proccessedTaskCount == 3 {
+			break
+		}
+		wg.Add(1)
+		go handleMessage(ctx, conn, mgr, wg)
 	}
-
+	time.Sleep(3 * time.Second)
+	cancel()
+	time.Sleep(time.Second)
+	fmt.Println("Main Goroutine exited")
+	wg.Wait()
+	fmt.Printf("All tasks are done. Total task is %v\n", mgr.proccessedTaskCount)
 }
 
-func handleMessage(conn net.Conn) {
-	defer conn.Close()
+func handleMessage(ctx context.Context, conn net.Conn, mgr *TaskMgr, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+		conn.Close()
+	}()
+
 	fmt.Println("Received message...")
+	mgr.Count()
+	fmt.Printf("Task Manager:: count %v\n", mgr.proccessedTaskCount)
 	buffer := make([]byte, 1024)
 	for {
-		fmt.Println(buffer)
+		// fmt.Println(buffer)
 		data, err := conn.Read(buffer)
 		if err != nil {
 			fmt.Println("Read buffer error:", err)
@@ -45,3 +82,10 @@ func handleMessage(conn net.Conn) {
 		fmt.Printf("[Server Response] %s\n", buffer[:data])
 	}
 }
+
+// shutdown TODO:: graceful shutdown
+func shutdown() {
+
+}
+
+func broadcast() {}
